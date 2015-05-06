449b6689a677530323149b69468c497cadeadc16
imrayya <imrayya@gmail.com>
2015-05-06 12:34:13 +0200

    Added Main Class

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+/**
+ * Runs the rest of the project
+ *
+ * @author Kareem Horstink
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
